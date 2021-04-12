e0009b6e12238d8511e3797c73a63afa1f84c0a3
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2021-04-12 11:46:57 -1000

    more proxy package cleanup

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -3,16 +3,12 @@ package proxy
 import (
 	"context"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"os"
 	"syscall"
-	"time"
 
-	"github.com/rs/zerolog"
 	"go.uber.org/zap"
 	"golang.org/x/crypto/ssh"
 )
@@ -67,7 +63,6 @@ func NewHTTPProxy(hostport string, remote string, conn *ssh.Client, logger *zap.
 				fmt.Fprintf(resp, "Fail!\n")
 				return
 			}
-			logger.Info("money!")
 			logger.Info(string(body))
 			fmt.Fprintf(resp, "Hello world!\n")
 		}),
@@ -81,9 +76,8 @@ func NewHTTPProxy(hostport string, remote string, conn *ssh.Client, logger *zap.
 	return &p
 }
 
-// Start starts the http listener
+// Start starts the http listener. This is blocking
 func (p *Proxy) Start() {
-
 	go func() {
 		// listener
 		if err := p.srv.ListenAndServe(); err != nil {
@@ -101,58 +95,4 @@ func (p *Proxy) Start() {
 // Stop stops the http proxy
 func (p *Proxy) Stop() {
 	p.done <- syscall.SIGSTOP
-}
-
-func madTCPProxyThing() {
-	var d net.Dialer
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	out, err := d.DialContext(ctx, "tcp", ":8888")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
-	}
-
-	l, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Listening on 8080")
-
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
-			logger := zerolog.New(out).With().Timestamp().Logger()
-			logger.Info().Msg("infoo!!")
-			// Echo all incoming data.
-			io.Copy(out, c)
-			c.Write([]byte(`message sent`))
-			// set SetReadDeadline
-			err := out.SetReadDeadline(time.Now().Add(5 * time.Second))
-			if err != nil {
-				fmt.Println("SetReadDeadline failed:", err)
-				// do something else, for example create new conn
-				return
-			}
-
-			recvBuf := make([]byte, 1024)
-			n, err := out.Read(recvBuf[:])
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			fmt.Println(n)
-			c.Write(recvBuf)
-			// Shut down the connection.
-			c.Close()
-		}(conn)
-	}
 }
