c8981e269f6a03bfb779861efa15cf184eece9cb
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2023-07-29 11:15:41 -0700

    remove unused method from main.go

--- cli/main.go
+++ cli/main.go
@@ -18,17 +18,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"io"
-	"log"
-	"net"
 	"os"
-	"time"
 
 	"github.com/mihai-chiorean/neti/cli/cmd"
 	"github.com/mihai-chiorean/neti/cli/config"
-	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
@@ -36,60 +30,6 @@ import (
 
 var cfgFile string
 var rootCmd *cobra.Command
-
-func madTCPProxyThing() {
-	var d net.Dialer
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	out, err := d.DialContext(ctx, "tcp", ":8888")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
-	}
-
-	l, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Listening on 8080")
-
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
-			logger := zerolog.New(out).With().Timestamp().Logger()
-			logger.Info().Msg("infoo!!")
-			// Echo all incoming data.
-			io.Copy(out, c)
-			c.Write([]byte(`message sent`))
-			// set SetReadDeadline
-			err := out.SetReadDeadline(time.Now().Add(5 * time.Second))
-			if err != nil {
-				fmt.Println("SetReadDeadline failed:", err)
-				// do something else, for example create new conn
-				return
-			}
-
-			recvBuf := make([]byte, 1024)
-			n, err := out.Read(recvBuf[:])
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			fmt.Println(n)
-			c.Write(recvBuf)
-			// Shut down the connection.
-			c.Close()
-		}(conn)
-	}
-}
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
